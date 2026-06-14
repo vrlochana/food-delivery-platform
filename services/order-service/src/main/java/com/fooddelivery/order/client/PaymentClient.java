ae36f6bd5e4c69333cc01d30473b6e781dc7ada7
vrlochana <vrlochana@gmail.com>
2026-06-14 14:38:07 +0530

    feat: implement service discovery with Eureka and Feign communication

---
+++ services/order-service/src/main/java/com/fooddelivery/order/client/PaymentClient.java
@@ -0,0 +1,2 @@
+package com.fooddelivery.order.client;public class PaymentClient {
+}
--- services/payment-service/src/main/java/com/fooddelivery/payment/client/OrderClient.java
+++ services/payment-service/src/main/java/com/fooddelivery/payment/client/OrderClient.java
@@ -4,10 +4,11 @@ import com.fooddelivery.payment.enums.PaymentStatus;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(
+/*@FeignClient(
         name = "order-service",
         url = "http://localhost:8084"
-)
+)*/
+@FeignClient(name = "ORDER-SERVICE")
 public interface OrderClient {
 
     @PutMapping("/api/v1/orders/{id}/status")
