4b385aa4fa695404a9dcaa83eb42bf936ce670e2
vrlochana <vrlochana@gmail.com>
2026-06-15 10:12:24 +0530

    feat: add config server and service discovery infrastructure

---
+++ config-server/src/test/java/com/fooddelivery/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fooddelivery.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
